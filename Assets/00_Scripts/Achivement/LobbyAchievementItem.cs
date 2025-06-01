7f12c31552615c734f21a4f485454962801c224b
vdlrwnsv12 <vdlrwnsv12@gmail.com>
2025-06-01 23:42:59 +0900

    Reapply "[Refactor] 도전과제 호출 불가/ 가능 서버 기준으로 검사"
    This reverts commit fd6fa17fc6e572651bbff4b8e3be9f8571758a39.

--- Assets/00_Scripts/Achivement/LobbyAchievementItem.cs
+++ Assets/00_Scripts/Achivement/LobbyAchievementItem.cs
@@ -1,26 +1,25 @@
 using UnityEngine;
 using UnityEngine.UI;
+using System.Threading.Tasks;
 
 public class LobbyAchievementItem : MonoBehaviour
 {
     public AchievementSO achievementSO;
-    
 
     [SerializeField] private Text titleText;
     [SerializeField] private Text descriptionText;
     [SerializeField] private GameObject badge;
     [SerializeField] private Slider slider;
 
-
     /// <summary>
-    /// 도전과제 정보 및 달성률 세팅
+    /// 도전과제 정보 및 달성 상태 세팅 (비동기)
     /// </summary>
-    public void InitData()
+    public async Task InitData()
     {
-        bool isCompleted = AchievementManager.Instance.IsAchievementCompleted(achievementSO.ID);
-        badge.SetActive(isCompleted);
-
         titleText.text = achievementSO.Name;
         descriptionText.text = achievementSO.Description;
+
+        bool isCompleted = await FirebaseManager.Instance.IsAchievementCompletedAsync(achievementSO.ID);
+        badge.SetActive(isCompleted);
     }
 }
